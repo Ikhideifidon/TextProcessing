75b4bfcddc29d7c92e891d1357d8f8c1c929d279
ifidonikhide <ikhideifidon@yahoo.com>
2022-11-30 01:00:39 -0500

    Text Processing

--- src/main/java/com/github/Ikhideifidon/TextProcess.java
+++ src/main/java/com/github/Ikhideifidon/TextProcess.java
@@ -1,2 +1,20 @@
-package com.github.Ikhideifidon;public class TextProcess {
+package com.github.Ikhideifidon;
+
+public class TextProcess {
+
+    // T(n) = O(mn) worst case
+    public static int findBruteForce(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+        if (m > n) return -1;
+        for (int i = 0; i <= m - n; i++) {
+            int index = 0;                  // Start index
+            while (index < n && text.charAt(i + index) == pattern.charAt(index))
+                index++;
+            if (index == n)
+                return i;
+        }
+        // Failed search
+        return -1;
+    }
 }
